16b2fe008c91ca15dfa391fb9cb2bc803b57b51c
Pascal488 <maxpascal03@gmail.com>
2022-09-28 00:50:36 +0300

    Adding a react-calender component

--- dashbord/src/components/Left.tsx
+++ dashbord/src/components/Left.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React,{ useState } from "react";
 
 
 import {BsBell} from 'react-icons/bs';
 
 import Image from "./images/Dashboard.jpeg";
 
-import {TbMessageDots} from 'react-icons/tb'
+import {TbMessageDots} from 'react-icons/tb';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const icon1 = <BsBell/>;
 
@@ -16,8 +19,9 @@ const icon2 = <TbMessageDots/>;
 
 
 const Left = () =>{
+    const [value, onChange] = useState(new Date());
     return(
-        <div className="bg-slate-400  flex  flex-col m-0 w-full fixed h-full overflow-auto  align-center text-xl gap-5">
+        <div className="bg-slate-500  flex  flex-col m-0 w-full fixed h-full overflow-auto  align-center text-xl gap-5">
             <div className="flex  justify-around  border-b-2 max-w-sm pb-5 p-2">
                 <span className="text-2xl w-11 h-11 border-2 border-white rounded-full p-2 cursor-pointer hover:rotate-45">
                     {icon1}
@@ -35,9 +39,11 @@ const Left = () =>{
             </div>  
 
 
-            <div className="">
-
+            <div className="p-1 text-center">
+            <Calendar onChange={onChange} value={value} className="max-w-[350px] p-1 "/>
             </div>
+
+
             </div>
     )
 }
