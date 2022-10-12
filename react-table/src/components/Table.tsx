20b70f36f98fa455842dedeefe520e8957f37e69
Pascal488 <maxpascal03@gmail.com>
2022-10-12 12:50:19 +0300

    Table init

---
+++ react-table/src/components/Table.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './table.css'
+
+const Table = () => {
+  return (
+    <table>
+        <tr>
+            <th>Hello</th>
+            <th>Hello</th>
+            <th>Hello</th>
+            <th>Hello</th>
+        </tr>
+        <tr>
+            <td>Juma</td>
+            <td>Hamisi</td>
+            <td>Shabani</td>
+            <td>Haruna</td>
+
+        </tr>
+    </table>
+  )
+}
+
+export default Table
